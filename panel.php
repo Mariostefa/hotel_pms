774d97459df64b3dfbd945c3dd635c64da1c6a15
Mariostefa <marios.stefa@gmail.com>
2023-12-28 00:48:31 +0200

    commit

--- panel.php
+++ panel.php
@@ -1,11 +1,12 @@
 <?php
     session_start();
     include 'db_connection.php';
+    include 'accesses.php';
     
 
     if($_SESSION['logged_in']){
-
-        echo "Welcome " . $_SESSION['user']. "</br>"; 
+        
+        echo "call function";
     
     }else{
         header("Location: index.php");
@@ -14,15 +15,30 @@
     
 ?>
 
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Panel</title>
+    <title>Controll Panel</title>
+    <link rel="stylesheet" href="css/navbar.css">
 </head>
 <body>
-<a href='logout.php'> Log out  </a>
+
+<div class="topnav">
+
+<a href="#news" >News</a>
+<a class="active" href="#home">Home</a>
+<a href="#contact">Contact</a>
+<a href="#about" class="split">Help</a>
+<a href="index.php" <?php session_destroy(); die(); ?>>logout</a>
+</div>
+
+<div style="padding-left:16px">
+<h2>Split Navigation Example</h2>
+<p style="text-align:center;"> <?php echo "Welcome " . $_SESSION['user'] ?> </p> </br>
+</div>
 </body>
 </html>
 
