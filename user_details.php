5653002e8afb9f2ad574b2c92012db04cb468592
Mariostefa <marios.stefa@gmail.com>
2023-12-27 04:24:47 +0200

    commit

--- user_details.php
+++ user_details.php
@@ -6,26 +6,42 @@
 
     function details ($id){
         
-        $data = request_data($id);
-
-
-        $_SESSION['afm'] = $data[3];          
-        $_SESSION['user_id'] = $id;
         $_SESSION['logged_in'] = true;
-        
 
+        //request a function that retrieves the afm for the session
+        $pr_key_yp = request_data($id , 0);
+        
+        $_SESSION['afm'] = $pr_key_yp[3];          
+        $_SESSION['user_id'] = $id;
+
+        //request a faction that retrieves some personal information and rights on the web services
+        //next move , needs to check on what rights exactly has.
+        $info_yp = request_data($id ,1);
+
+        $_SESSION['user'] = $info_yp[1] ;
+        $_SESSION['epitheto'] = $info_yp[2];
+        $_SESSION['dikaiwma'] = $info_yp[9];
+
+        //Redirects on panel.php
         header("Location: panel.php");
     }
 
     $retunred = details(15);
 
-    function request_data($id){
+    function request_data($id , $unique){
         
         global $conne;
-
-        $query_login = "SELECT * FROM stoixia_sundeshs WHERE kwdikos = $id ";
-        $result = mysqli_query($conne , $query_login) ;
-        $row= mysqli_fetch_row($result);
+        if($unique == 0){
+            $query_login = "SELECT * FROM stoixia_sundeshs WHERE kwdikos = $id ";
+            $result = mysqli_query($conne , $query_login) ;
+            $row= mysqli_fetch_row($result);        
+        }
+        else{
+            $afm = $_SESSION['afm'];
+            $query_login = "SELECT * FROM ypallhlos WHERE  afm = $afm ";
+            $result = mysqli_query($conne , $query_login) ;
+            $row= mysqli_fetch_row($result);    
+        }
 
         return $row;
 
