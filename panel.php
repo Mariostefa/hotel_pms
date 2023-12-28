ac285d54cbc1349ec2791a1fc7090eb35587b544
Mariostefa <marios.stefa@gmail.com>
2023-12-28 03:08:53 +0200

    commit

--- panel.php
+++ panel.php
@@ -28,16 +28,16 @@
 
 <div class="topnav">
 
-<a href="#news" >News</a>
-<a class="active" href="#home">Home</a>
-<a href="#contact">Contact</a>
-<a href="#about" class="split">Help</a>
-<a href="index.php" <?php session_destroy(); die(); ?>>logout</a>
+    <a href="#news" >News</a>
+    <a class="active" href="#home">Home</a>
+    <a href="#contact">Contact</a>
+    <a href="#about" class="split">Help</a>
+    <a href="logout.php">logout</a>
 </div>
 
 <div style="padding-left:16px">
-<h2>Split Navigation Example</h2>
-<p style="text-align:center;"> <?php echo "Welcome " . $_SESSION['user'] ?> </p> </br>
+    <h2>Split Navigation Example</h2>
+    <p class="intro"> <?php echo "Welcome " . $_SESSION['user'] ?> </p> </br>
 </div>
 </body>
 </html>
