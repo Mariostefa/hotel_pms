eac7b33e2eb56ed5ce0f40c1fafc7e472589cabf
Mariostefa <marios.stefa@gmail.com>
2024-02-14 01:29:33 +0200

    abort _mission_github_does_support_php

--- front_page/index.php
+++ front_page/index.php
@@ -11,31 +11,21 @@
     </style>
 </head>
 <body>
-<header>
-    <div class="topnav">
-    <h2><a href="index.php">DIPAE hotel</a></h2>
-    <nav>
-        <ul>
-            <li><a href="front_page/services.php">Υπηρεσίες</a></li>
-            <!-- <li><a href="excursions.php">Εκδρομές</a></li> -->
-            <li><a href="front_page/bookings.php">Κράτηση</a></li>
-            <li><a href="front_page/destroy_session_variables.php">Σύνδεση</a></li>
-        </ul>
-    </nav>
-    </div>
-</header>
+    <?php
+    include("header.html");
+    ?>
     <main>
         <p>Καλώς ήρθατε στην κεντρική σελίδα του DIPAE HOTEL</p>
-        <img src="front_page/images/Dipae_Hotel_Main_Image.jpg" alt="Dipae hotel main page image">
+        <img src="images/Dipae_Hotel_Main_Image.jpg" alt="Dipae hotel main page image">
         <div class="map">
            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1753.950481567304!2d24.37940779515262!3d40.92948984986944!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14aeba5320b72bd1%3A0xc30420853816ff38!2sInternational%20Hellenic%20University!5e0!3m2!1sen!2sgr!4v1702839029037!5m2!1sen!2sgr" 
             width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>" 
         <div>
     </main>
-<footer>
-    <p>Uncopyrighted by Omada 4. Last built January 7th, 2024.</p>
-    <p>Hotel-dipae &copy;2023</p>
-</footer>
+<?php
+    include("footer.html");
+    ?>
+
 
     
 </body>
